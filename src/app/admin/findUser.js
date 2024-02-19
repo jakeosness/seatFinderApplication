31c33392eb17500459fbfe556103b8946176ed28
ShawnM999 <azmelonis@gmail.com>
2024-02-19 10:59:41 -0700

    Added admin folder with find user html and ccs

---
+++ src/app/admin/findUser.js
@@ -0,0 +1,39 @@
+
+
+<div>
+<div class="header">
+    <h1>SeatFinder</h1>
+    <div class="buttons">
+        <button id="home">Home</button>
+        <button id ="notify">Notifications</button>
+    </div>
+</div>
+
+{/*Page content here*/}
+<br></br>
+
+<h1 class="find">Find User</h1>
+  
+  <hr></hr>
+  
+  <h3 class="sub-header">Enter Username Below</h3>
+
+  
+<input type="text" id="username-input" placeholder="Enter Username"></input>
+  
+<button id="search">Search</button>
+
+  <div id="content" style="display: none;">
+    <div class="user-info">
+        <h2 id="user-name">Sir Tester</h2>
+        <p id="user-email">testiscool@gmail.com</p>
+        <p id="user-phone">555-555-5555</p>
+    </div>
+  </div>
+     
+
+  
+<div class="footer">
+    <p>&copy; 2024 SeatFinder. All rights reserved.</p>
+</div>
+</div>
