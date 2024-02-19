65a1007dda09b250035645437b090cd1605ead54
ppontera <ppontera@asu.edu>
2024-02-19 11:01:27 -0700

    Added adminDelete folder and page.js

--- src/app/appLogin/appHome/appAdmin/page.js
+++ src/app/appLogin/appHome/appAdmin/page.js
@@ -5,14 +5,19 @@ import { useRouter } from 'next/navigation';
 const appAdmin = () => {
     const router = useRouter();
 
-    // const profileRoute = () => {
-    //     router.push('/profile')
-    // };
+    const profileRoute = () => {
+        router.push('/appLogin/appHome/appAdmin/adminDelete')
+    };
+
+    const profileRouteTwo = () => {
+      router.push('/appLogin/appHome/appAdmin/adminFind')
+  };
   return (
         <div>
           <h1 style={{ textAlign: 'center', fontSize: '100px' }}>ASU Seat-Finder</h1>
           <div style= {{ textAlign: 'center' }}>
-          <button style={{ width:'200px', borderRadius: '8px', padding: '10px' }}>Find User</button>
+          <button onclick={profileRouteTwo} style={{ width:'200px', borderRadius: '8px', padding: '10px' }}>Find User</button>
+          <button onclick={profileRoute} style={{ width:'200px', borderRadius: '8px', padding: '10px' }}>Delete User</button>
           </div>
           
         </div>
